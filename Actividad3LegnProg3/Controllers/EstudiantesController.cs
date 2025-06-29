9ab3837b2a5090fda224ab8fbc85c9249097620c
alexis0314 <alexismercedes365@gmail.com>
2025-06-28 20:03:48 -0400

    Agregar archivos de proyecto.

---
+++ Actividad3LegnProg3/Controllers/EstudiantesController.cs
@@ -0,0 +1,87 @@
+﻿using Actividad4LegnProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Win32;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+
+namespace AcAtividad3LegnProg3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private static List<EstudianteViewModel> Estudiante = new List<EstudianteViewModel>()
+    {
+        new EstudianteViewModel() { Nombrecompleto = "Alexis", Matricula = "1234", Carrera = "Ingeniería en Software" }
+    };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            return View(Estudiante); 
+        }
+
+        [HttpGet]
+        public IActionResult Registro()
+        {
+            ViewBag.Carrera = new List<string> { "Ingeniería en Software", "Contabilidad", "Derecho" };
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registro(EstudianteViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Estudiante.Add(model);
+                TempData["Mensaje"] = "El estudiante se ha registrado exitosamente";
+                return RedirectToAction("Registro");
+            }
+
+            ViewBag.Carrera = new List<string> { "Ingeniería en Software", "Contabilidad", "Derecho" };
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            if (id >= 0 && id < Estudiante.Count)
+            {
+                var estudiante = Estudiante[id];
+                ViewBag.Carrera = new List<string> { "Ingeniería en Software", "Contabilidad", "Derecho" };
+                ViewBag.Id = id;
+                return View("Editar", estudiante);
+            }
+            return RedirectToAction("Lista");
+        }
+
+        [HttpPost]
+        public IActionResult Editar(int id, EstudianteViewModel model)
+        {
+            if (ModelState.IsValid && id >= 0 && id < Estudiante.Count)
+            {
+                Estudiante[id] = model;
+                TempData["Mensaje"] = "Estudiante actualizado";
+                return RedirectToAction("Lista");
+            }
+            ViewBag.Carrera = new List<string> { "Ingeniería en Software", "Contabilidad", "Derecho" };
+            return View("Editar", model);
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            if (id >= 0 && id < Estudiante.Count)
+            {
+                Estudiante.RemoveAt(id);
+                TempData["Mensaje"] = "Estudiante eliminado";
+            }
+            return RedirectToAction("Lista");
+        }
+
+    }
+}
---
+++ Actividad3LegnProg3/Models/EstudianteViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LegnProg3.Models
+{
+    public class EstudianteViewModel
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre está incompleto")]
+        public string Nombrecompleto { get; set; }
+
+        [Required(ErrorMessage = "La matrícula es obligatoria")]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "La matrícula debe tener entre 6 y 15 caracteres.")]
+        public string Matricula { get; set; }
+
+        [Required(ErrorMessage = "La carrera es obligatoria")]
+        public string Carrera { get; set; }
+
+        [Required(ErrorMessage = "El correo institucional es obligatorio")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un correo electrónico válido")]
+        public string CorreoInstitucional { get; set; }
+
+        [Phone(ErrorMessage = "Debe ingresar un número de teléfono válido")]
+        [MinLength(10, ErrorMessage = "El teléfono debe tener al menos 10 dígitos")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria")]
+        [DataType(DataType.Date)]
+        public DateTime Fechadenacimiento { get; set; }
+
+        [Required(ErrorMessage = "El género es obligatorio")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "El turno es obligatorio")]
+        public string Turno { get; set; }
+
+        [Required(ErrorMessage = "El tipo de ingreso es obligatorio")]
+        public string Tipodeingreso { get; set; }
+
+        public bool Estabecado { get; set; }
+
+        [Range(0, 100, ErrorMessage = "El porcentaje de beca debe estar entre 0 y 100")]
+        public int? Porcentajedebeca { get; set; }
+
+        [Required(ErrorMessage = "Debe aceptar los términos y condiciones")]
+        [Display(Name= "Terminosycondiciones")]
+        public bool Terminosycondiciones { get; set; }
+    }
+}
+
