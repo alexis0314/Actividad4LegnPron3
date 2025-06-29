202148192a9b2b5578ed9bf5447ffc3d16d54ab7
alexis0314 <alexismercedes365@gmail.com>
2025-06-28 21:49:09 -0400

    Creacion de los Modelos

---
+++ Actividad3LegnProg3/Models/CalificacionesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LegnProg3.Models
+{
+    public class CalificacionesViewModel
+    {
+        [Required(ErrorMessage = "La matrícula es obligatoria")]
+        public int MatriculaEstudiantes { get; set; }
+
+        [Required]
+        public int CodigoMateria { get; set; }
+
+        [Required]
+        [StringLength(0, MinimumLength = 100, ErrorMessage = "La Nota debe tener entre 0 y 100 caracteres.")]
+        public decimal Nota { get; set; }
+
+        [Required]
+        public int Periodo { get; set; }
+
+    }
+}
---
+++ Actividad3LegnProg3/Models/MateriasViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LegnProg3.Models
+{
+    public class MateriasViewModel
+    {
+        [Required]
+        public int Codigo { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El nombre está incompleto")]
+        public string Nombre { get; set;}
+
+        [Required]
+        public int Credito { get; set; }
+
+        [Required]
+        [StringLength(1, MinimumLength = 10, ErrorMessage = "La Carrera debe tener entre 1 y 10 caracteres.")]
+        public string Carrera { get; set; }
+    }
+}
